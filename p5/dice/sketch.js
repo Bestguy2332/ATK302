17622970250054ac101b8dead7677ca9b226ce43
Bestguy2332 <54325478+Bestguy2332@users.noreply.github.com>
2019-10-01 11:56:12 -0500

    o

--- p5/dice/sketch.js
+++ p5/dice/sketch.js
@@ -1,6 +1,9 @@
 var timer = 0;
 var myState = 0;
-var roll = 1;
+var roll = 0;
+var x = 0;
+var result = 1000;
+var timer2 = 0;
 function setup() {
   createCanvas (windowWidth, windowHeight);
   // put setup code here
@@ -56,24 +59,73 @@ function draw() {
     ellipse(windowWidth*2/3, windowHeight/2, 100, 100);
     break;
   }
-timer = timer + 1
-  if (timer >= roll) {
-    timer = 0;
-    myState = 1 + myState;
-  }
+
+  switch(roll) {
+  case 0:
+  result = 1;
+
+  break;
+
+  case 1:
+  result = 5;
+  break;
+
+  case 2:
+  result = 25;
+  break;
+
+  case 3:
+  result = 50;
+  break;
+
+  case 4:
+  result = 100;
+  break;
+
+  case 5:
+  result = 10000;
+  break;
+
+  case 6:
+  result = 1;
+
+  break;
+}
+timer = timer + 1;
+timer2 = timer2 + 1;
+
+if (timer > result){
+  timer = 0;
+  myState = myState + 1;
+}
+
+if (timer2 > 100) {
+  roll = roll + 1;
+  timer2 = 0;
+}
 
   if (myState > 5){
     myState = 0;
   }
 
-  if (roll > 100) {
-    roll = 0;
-  }
+ if (result > 200) {
+   result = 0;
+ }
+if (roll > 5) {
+  result = 10000;
+}
 }
 
-function mouseReleased () {
- if (roll = 0) {
-   roll = 1;
- }
+function mousePressed () {
+
+roll = 0;
+timer2 = 0;
 
  }
+
+ function deviceShaken  () {
+
+ roll = 0;
+ timer2 = 0;
+
+  }
--- p5/week6stoplight/sketch.js
+++ p5/week6stoplight/sketch.js
@@ -1,6 +1,7 @@
 var x = 0;
-
+var vel = 0;
 var myState = 0;
+var y = 1;
 
 function setup() {
   createCanvas (windowWidth, windowHeight)
@@ -24,6 +25,7 @@ rect (500,100,300,500)
 fill('red')
 
 ellipse(650, 200, 100, 100)
+vel = 0;
     break;
 
     case 1:
@@ -33,6 +35,7 @@ rect (500,100,300,500)
 fill('green')
 
 ellipse(650, 500, 100, 100)
+vel = 5;
     break;
 
     case 2:
@@ -41,10 +44,12 @@ fill(0)
 rect (500,100,300,500)
 fill('yellow')
 ellipse(650, 350, 100, 100)
+vel = 1;
     break;
-
-
 }
+y = y + vel;
+fill ('purple')
+rect( y + vel ,height - 100, 200, 100)
 
 if (x > 200) {
   x = 0;
@@ -55,4 +60,7 @@ if (myState > 2) {
   myState = 0
 }
 
+if (y > windowWidth) {
+  y = 0;
+}
 }
