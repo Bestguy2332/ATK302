99310cdc8c6aa57b5920b8edca598ec111a2de68
Bestguy2332 <54325478+Bestguy2332@users.noreply.github.com>
2019-11-05 12:49:19 -0600

    o

--- p5/googledatamy/sketch.js
+++ p5/googledatamy/sketch.js
@@ -1,9 +1,9 @@
 var namesArray = [];
-
+var vel = 0;
 function setup() {
 
   // Tabletop stuff, for getting google spreadsheet data in.
-  let url = '1GtE3eoYVWBv9zMPoyettXzDCEv6qdNGKio_hgEh5duM'; // this is KEY of the URL from the sheet
+  let url = '1cAu6qZDwwvCML12UB8LRyfY0zeH11_-Ol-BwEMzsDqc'; // this is KEY of the URL from the sheet
   let settings = {
     key: url, // The url of the published google sheet
     callback: gotData, // A callback for when the data comes in
@@ -15,7 +15,7 @@ function setup() {
 
 
   // Regular setup code we usually have
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   textAlign(CENTER);
   ellipseMode(CENTER);
   rectMode(CENTER);
@@ -30,43 +30,71 @@ function gotData(data) {
 
   // iterate through the array of data and create an object and push it on an array called namesArray
   for (let i = 0; i < data.length; i++) {
-    namesArray.push(new Circle(data[i].Name, data[i].Shape)) ;
+    namesArray.push(new Circle(data[i].name, data[i].color)) ;
    }
 
 }
 
 
 function draw() {
-  background('blue');
+  background(0);
 
   // // iterate through the namesArray and display the objects!
   for (let i = 0 ; i < namesArray.length ; i++) {
     namesArray[i].display() ;
+    namesArray[i].drive();
+
+
   }
 
 }
 
 
 // my circle class
-function Circle(myName, myShape) {
+function Circle(myName, myColor) {
   this.pos = createVector(random(width), random(height));
+  this.vel = createVector(random(-5, 5), random(-5, 5));
   this.name = myName;
-  this.shape = myShape;
+  this.color = myColor;
+
 
 
   this.display = function() {
-    if (this.shape === 'Circle') {
-    fill(255);
+
+    if (myColor === 'red') {
+    fill(255, 0,  0);
     ellipse(this.pos.x, this.pos.y, 100, 100);
   }
-   if (this.shape === 'Square') {
-     fill(255);
-     rect(this.pos.x, this.pos.y, 100, 100);
+   if (myColor === 'orange') {
+     fill(255, 127, 0);
+     ellipse(this.pos.x, this.pos.y, 100, 100);
    }
+   if (myColor === 'yellow') {
+   fill(255, 255,  0);
+   ellipse(this.pos.x, this.pos.y, 100, 100);
+ }
+ if (myColor === 'green') {
+ fill(0, 255,  0);
+ ellipse(this.pos.x, this.pos.y, 100, 100);
+}
+if (myColor === 'blue') {
+fill(0,0,255);
+ellipse(this.pos.x, this.pos.y, 100, 100);
+}
+if (myColor === 'purple') {
+fill(255, 0,  255);
+ellipse(this.pos.x, this.pos.y, 100, 100);
+}
     fill(0);
     text(myName, this.pos.x, this.pos.y);
 
-
+    this.drive = function() {
+      this.pos.add(this.vel);
+      if (this.pos.x > width) this.pos.x = 0;
+      if (this.pos.x < 0) this.pos.x = width;
+      if (this.pos.y > height) this.pos.y = 0;
+      if (this.pos.y < 0) this.pos.y = height;
+    }
 
   }
 
